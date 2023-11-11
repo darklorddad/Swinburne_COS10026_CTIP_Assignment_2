28e56d55c6441bc702cee470ba5f63834c9f3b94
kelly-tan2608 <kellyttl2608@gmail.com>
2023-11-11 22:21:22 +0800

    ttl 38

--- www/assign2/view_user.php
+++ www/assign2/view_user.php
@@ -2,8 +2,8 @@
 <html lang = "en">
 <head>
     <meta name = "author" content = "Tan Tzy Lynn">
-    <meta name = "description" content = "View User Account">
-    <title>FLORASCAN - View User Account</title>
+    <meta name = "description" content = "User Management">
+    <title>FLORASCAN - User Management</title>
     <?php include_once("head.php"); ?>
 </head>
 <body class = "ttl_body">
@@ -29,49 +29,52 @@
 
     <div class="ash_section_1">
 		<div class="ash_page_header">
-			<p class = "ash_title_header">View User Account</p>
+			<p class = "ash_title_header">User Management</p>
 		</div>
 	</div>
 
     <div class = "ttl_main_body">
+        <div class="ttl_view_user_table_container">
+            <table class="ttl_view_user_table">
+                <tr>
+                    <th class="ttl_view_user_table_heading">Username</th>
+                    <th class="ttl_view_user_table_heading">Email</th>
+                    <th class="ttl_view_user_table_heading">Password</th>
+                    <th class="ttl_view_user_table_heading">Action</th>
+                </tr>
 
-        <table class="ttl_view_user_table">
-            <tr>
-                <th class="ttl_view_user_table_heading">Username</th>
-                <th class="ttl_view_user_table_heading">Email</th>
-                <th class="ttl_view_user_table_heading">Password</th>
-                <th class="ttl_view_user_table_heading">Action</th>
-            </tr>
+                <?php
+                    $admin_username = "Admin";
 
-            <?php
-                $admin_username = "Admin";
+                    // Fetch the next row of a result set as an associative array
+                    while ($res = mysqli_fetch_assoc($result)) {
+                        echo "<tr>";
+                        echo "<td class='ttl_view_user_table_row'>".$res['username']."</td>";
+                        echo "<td class='ttl_view_user_table_row'>".$res['email']."</td>";
+                        echo "<td class='ttl_view_user_table_row'>".$res['user_password']."</td>";
 
-                // Fetch the next row of a result set as an associative array
-                while ($res = mysqli_fetch_assoc($result)) {
-                    echo "<tr>";
-                    echo "<td>".$res['username']."</td>";
-                    echo "<td>".$res['email']."</td>";
-                    echo "<td>".$res['user_password']."</td>";
+                        
+                        if (isset($res['username']) && $res['username'] == $admin_username) {
+                            // Display 'Edit' only for the user with the matching username
+                            echo "<td class='ttl_view_user_table_row'><a href=\"edit_user.php?id=$res[id]\" class='ttl_edit'>Edit</a></td>";
+                        } else {
+                            // Display both 'Edit' and 'Delete' for other users
+                            echo "<td class='ttl_view_user_table_row'><a href=\"edit_user.php?id=$res[id]\" class='ttl_edit'>Edit</a> | <a href=\"delete_user.php?id=$res[id]\" class='ttl_delete'>Delete</a></td>";
+                        }
 
-                    
-                    if (isset($res['username']) && $res['username'] == $admin_username) {
-                        // Display 'Edit' only for the user with the matching username
-                        echo "<td><a href=\"edit_user.php?id=$res[id]\">Edit</a></td>";
-                    } else {
-                        // Display both 'Edit' and 'Delete' for other users
-                        echo "<td><a href=\"edit_user.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\">Delete</a></td>";
+                        echo "</tr>";
                     }
-
-                    echo "</tr>";
-                }
-            ?>
+                ?>
 
 
-        </table>
+            </table>
+        </div>
 
-        <a href = "create_user.php">
-            <button class = 'ttl_create_user_account' type='button'>Create New User Account</button>
-        </a>
+        <div class="ttl_create_user_account_container">
+            <a href = "create_user.php" class = 'ttl_create_user_account_btn'>
+                Create New User Account
+            </a>
+        </div>
 
     </div>
 
