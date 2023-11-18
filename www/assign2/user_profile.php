910eef328db25b39c2aa97b8bfd3bf221ac54b70
ashleyjong <129250977+ashleyjong@users.noreply.github.com>
2023-11-18 10:49:00 +0800

    ash 61 temp

---
+++ www/assign2/user_profile.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta name = "author" content = "Ashley">
+    <meta name = "description" content = "Create User Account">
+    <title>FLORASCAN - View User Profile</title>
+    <?php include_once("head.php"); ?>
+</head>
+<body class="ash_body">
+    <?php include_once("dld_background_dots.php"); ?>
+
+    <div class="ash_section_1">
+		<div class="ash_page_header">
+			<p class = "ash_title_header">User Profile</p>
+		</div>
+	</div>
+
+    
+</body>
+
+
+</html>
