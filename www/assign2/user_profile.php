dd5de885a0393de8b6584fe484cfb7ffbccce188
ashleyjong <129250977+ashleyjong@users.noreply.github.com>
2023-12-02 00:18:12 +0800

    ash 62 temp

--- www/assign2/user_profile.php
+++ www/assign2/user_profile.php
@@ -14,5 +14,20 @@
 			<p class = "ash_title_header">User Profile</p>
 		</div>
 	</div>
+<<<<<<< Updated upstream
+=======
+
+    <div class="ash_container_1">
+        <div class="ash_profile_box">
+            <!-- <img src="" class="ash_menu_icon">
+            <img src="" class="ash_setting_icon"> -->
+            <img src="styles/images/ash/ash_plant_icon_1.jpg" alt="ash plant icon" class="ash_profile_pic">
+            <h3>Admin</h3>
+            <p>Admin account of FLORASCAN</p>
+        </div>
+    </div>
+    
+
+>>>>>>> Stashed changes
 </body>
 </html>
