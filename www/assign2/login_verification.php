373ef95335f89f4bdbcc5ea9fd9610a69e2adf91
darklorddad <danieltiong000@gmail.com>
2023-12-07 23:00:53 +0800

    2.20 account linkage fix

--- www/assign2/login_verification.php
+++ www/assign2/login_verification.php
@@ -1,23 +1,27 @@
 <?php
     session_start(); 
+
+    $hostname = "127.0.0.1";
+    $user = "root";
+    $password = "";
+    $database = "florascan_database";
+
+    $conn = mysqli_connect($hostname, $user, $password, $database);
+    
     include("user_table.php");
     createUserTable();
 
-    if (isset($_SESSION["user_id"]) ) {
-    }
-    else {
-        $_SESSION["user_id"] = null;
-    }
-    
-    if (isset($_SESSION["user_id"]) ) {
+    session_start();
+
+    if (isset($_SESSION['user_id'])) {
+        $result = mysqli_query($conn, "SELECT * FROM UserDetails WHERE user_id = " . $_SESSION['user_id']);
         if ($_SESSION["user_id"] == "000001") {
             header("Location: view_user.php");
         }
         else {
             header("Location: user_profile.php");
         }
-    }
-    else {
+    } else {
         header("Location: login.php");
     }
 ?>
